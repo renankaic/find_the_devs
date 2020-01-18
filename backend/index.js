6ca3328d3b10f96b89258ce86f2a8a431bb49846
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-01-18 16:04:28 -0300

    Explaining HTTP methods

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,28 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
+// HTTPS methods GET, POST, PUT, DELETE
+
+////Params types:
+//Query Params:
+//Used in GET requests
+//request.query (filters, ordenation, pagination...)
+
+////Route Params:
+//  /{resource}/:id
+//  Used to identify a resource to change or to delete
+//  request.params
+
+////Body:
+// Used to create or update a resource
+// /{resource}
+// { "name": "John", "age": 13}
+
 app.get('/', (request, response) => {
 
+    console.log(request.query);
     return response.json({ message: 'Hello Renan' })
 
 })

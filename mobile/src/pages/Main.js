5d0dbe5758f61ab2322f10db2da8b88e25778f55
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-01-19 12:39:46 -0300

    Feat - Using WebView component to show the Dev's Github Profile after clicking on his description in the map

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -3,7 +3,7 @@ import { StyleSheet, Image, View, Text } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-function Main() {
+function Main({ navigation }) {
 
     const [currentRegion, setCurrentRegion ] = useState(null);
 
@@ -44,7 +44,9 @@ function Main() {
             <Marker coordinate={{ latitude: -22.7505755, longitude: -47.6016348 }}>
                 <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/26421325?s=400&u=7fd8f43279c50741ef0d6b2a8a7f151c28b72d4b&v=4' }} />
 
-                <Callout>
+                <Callout onPress={() => {
+                    navigation.navigate('Profile', { github_username: 'renankaic' })
+                }}>
                     <View style={styles.callout}>
                         <Text style={styles.devName}>Renan Kaic Lopes</Text>
                         <Text style={styles.devBio}>System Analyst at Hyundai AutoEver Brazil. In love with programming</Text>

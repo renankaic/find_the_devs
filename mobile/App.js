0dfea08b96f9db53d4793de1cfa70bd56543e795
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-01-19 12:02:31 -0300

    Feat - Creating first views with React Native libraries. Using MapView

--- mobile/App.js
+++ mobile/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello OmniStack!!!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
+      <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    fontWeight: 'bold',
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontWeight: 'bold',
-    fontSize: 32,
-    color: '#FFF'
-  }
-});
